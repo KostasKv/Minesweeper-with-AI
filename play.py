4b0e5ba4766c1722a3bb6519fafaffb90ef5a0a0
KostasKv <kostas.kvietinskas@gmail.com>
2021-03-19 18:09:15 +0000

    play.py script easy toggle for user to play added

--- play.py
+++ play.py
@@ -1,5 +1,6 @@
 import time
 import cProfile
+import random
 
 from minesweeper_ai import minesweeper
 from minesweeper_ai.agents.random_agent import RandomAgent
@@ -7,22 +8,33 @@ from minesweeper_ai.agents.random_legal_moves_agent import RandomLegalMovesAgent
 from minesweeper_ai.agents.pick_first_uncovered_agent import PickFirstUncoveredAgent
 from minesweeper_ai.agents.no_unnecessary_guess_solver import NoUnnecessaryGuessSolver
 from minesweeper_ai.agents.cbr_agent1 import CBRAgent1
+from bitarray import bitarray
 
 def main():
     main_play()
 
 def main_play():
+    game_seeds = None
+
+    human_player = True
     # Constants (configurables)
     profile = False
     benchmark = False
     num_games_profile = 100
     num_games_benchmark = 10
-    num_games_other = 2
-    config = {'rows': 16, 'columns': 30, 'num_mines': 99, 'first_click_is_zero': True}
-    run_seed = 54
-    agent_seed = 13
+    num_games_other = 10
+    # config = {'rows': 16, 'columns': 30, 'num_mines': 99, 'first_click_is_zero': True}
+    config = {'rows': 8, 'columns': 8, 'num_mines': 10, 'first_click_is_zero': False}
+    # run_seed = 54
+    # agent_seed = 13
+    run_seed = 40   # Same run seed as main experiment
+    agent_seed = 4040   # Same agent seed as main experiment
     # game_seeds = [7083311470311291716, 6969] * 3
-    game_seeds = None
+    # game_seeds = [-8635908151614172643, 3031145623968078003, 4833213684712801012, 5882193735102876406, -3445616753041698909]
+    # game_seeds = [-8635908151614172643, 3031145623968078003, 4833213684712801012, 5882193735102876406, -3445616753041698909, 8588763334019272104, 8777523799321782054, 6959701420644399351, 5958711173367462203, 4731185150046434087]
+    game_seeds = [4731185150046434087]
+    random.shuffle(game_seeds)
+    print(game_seeds)
     sample_size = (5, 5)
     
     # Solvers
@@ -67,12 +79,49 @@ def main_play():
         # minesweeper.run(verbose=0, config=config, seed=57, visualise=True)
         # run(random_agent, config=config, verbose=True, visualise=True)
         # run(random_legal_agent, config=config, visualise=True, verbose=False, num_games=50, seed=57)
+        if human_player:
+            solver_agent = None
+        
         results = minesweeper.run(solver_agent, config=config, visualise=True, verbose=False, num_games=num_games_other, seed=run_seed, game_seeds=game_seeds)
         # run(cbr_agent_1, visualise=True, verbose=True, num_games=10)
+        
 
-        for game_stats in results['games']:
+        all_binary_grid_mines = [
+            '0x0020008650008060100000',
+            '0x00C4300008140802000000',
+            '0x04801180000008008A0080',
+            '0x18000002060001C0042000',
+            '0x2000000110144002004500',
+            '0x00002000020062D0300000',
+            '0x2140000020110201100800',
+            '0x0024680044800060000000',
+            '0x0020000008600C00143000',
+            '0x4088280000200000005300',
+        ]
+
+        for (i, game_stats) in enumerate(sorted(results['games'], key=lambda x: x['seed'])):
+            binary_grid_mines = all_binary_grid_mines[i]
+            binary_grid_mines = binary_grid_mines[2:]   # trim 0x prefix
+            x = bytes.fromhex(binary_grid_mines)
+            
+            y = grid_to_binary(game_stats['grid_mines'])
+
             print(f"({game_stats['first_click_pos_x']}, {game_stats['first_click_pos_y']})\t{game_stats['seed']}" )
+            print(f"{x} == {y} : {x == y}\n")
     print("Program stopped.")
+
+def grid_to_binary(grid):
+    binary_grid = bitarray()
+
+    for row in grid:
+        for tile in row:
+            if tile.is_mine:
+                binary_grid.append(True)
+            else:
+                binary_grid.append(False)
+
+    return binary_grid.tobytes()
+
 
 if __name__ == '__main__':
     main()
